1384070518c1a1bc9691d18d7b54149e7896736f
Nora <ncos@protonmail.ch>
2020-11-14 21:38:08 -0500

    slugma

--- src/main/java/zone/nora/emojis/Emojis.java
+++ src/main/java/zone/nora/emojis/Emojis.java
@@ -1,11 +1,35 @@
 package zone.nora.emojis;
 
-import com.vdurmont.emoji.EmojiManager;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Emojis {
-    public static void main(String[] args) {
-        EmojiManager.getAll().forEach(it -> {
-            System.out.println(it.getUnicode());󠁮󠁧󠁿
+    public static void main(String[] args) throws IOException {
+        File input = new File("emojis.json");
+        String contents = FileUtils.readFileToString(input);
+        JsonObject obj = new JsonParser().parse(contents).getAsJsonObject();
+        JsonArray newJson = new JsonArray();
+        obj.entrySet().forEach(it -> {
+            JsonArray array = it.getValue().getAsJsonArray();
+            array.forEach(it_ -> {
+                JsonObject emoji = it_.getAsJsonObject();
+                String emojiName = emoji.get("name").getAsString().replace('-', '_');
+                emoji.remove("name");
+                emoji.addProperty("name", emojiName);
+                newJson.add(emoji);
+                System.out.println(emojiName);
+            });
         });
+        File output = new File("output.json");
+        if (output.createNewFile()) {
+            FileUtils.writeStringToFile(output, newJson.toString());
+        } else {
+            System.out.println(newJson.toString());
+        }
     }
 }
